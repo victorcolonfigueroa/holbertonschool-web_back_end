b6824ba532b4d9279d4323a48dd5b9f361596b13
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-07-18 15:00:24 -0400

    Refactor initializeRooms function to use consistent indentation

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,12 @@
 import ClassRoom from './0-classroom';
 
 function initializeRooms() {
-  return [
-	new ClassRoom(19),
-	new ClassRoom(20),
-	new ClassRoom(34)
-  ];
+	return [
+		new ClassRoom(19),
+		new ClassRoom(20),
+		new ClassRoom(34)
+	];
 }
 
-initializeRooms();
+export default initializeRooms; // Exporting the function
+
