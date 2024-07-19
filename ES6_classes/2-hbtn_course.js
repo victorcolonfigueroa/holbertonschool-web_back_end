3d12b4369d88bd3f6f2e563c431d860a6ece62f6
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-07-19 11:55:05 -0400

    Refactor HolbertonCourse class to use consistent indentation

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,48 +1,59 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (typeof length !== 'number') throw new TypeError('Legnth must be a number');
-    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('Student must be an array of strings');
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this._name = this._validateName(name);
+    this._length = this._validateLength(length);
+    this._students = this._validateStudents(students);
   }
 
+  // Validate name
+  _validateName(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    return this._name;
+  }
+
+  // Validate length
+  _validateLength(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    return this._length;
+  }
+
+  _validateStudents(students) {
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    // Example of using 'this' in a class method, even if not needed for validation
+    this.lastValidationDate = new Date();
+    return students;
+  }
+
+  // Getter and setter for name
   get name() {
     return this._name;
   }
 
   set name(newName) {
-    if (typeof newName === 'string' && newName.length > 0) {
-      this._name = newName;
-    } else {
-      console.error('Invalid name');
-    }
+    this._name = this._validateName(newName);
   }
 
+  // Getter and setter for length
   get length() {
     return this._length;
   }
 
   set length(newLength) {
-    if (typeof newLength === 'number' && newLength > 0) {
-      this._length = newLength;
-    } else {
-      console.error('Invalid length');
-    }
+    this._length = this._validateLength(newLength);
   }
 
+  // Getter and setter for students
   get students() {
     return this._students;
   }
 
   set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
-    } else {
-      console.error('Invalid students array');
-    }
+    this._students = this._validateStudents(newStudents);
   }
 }
