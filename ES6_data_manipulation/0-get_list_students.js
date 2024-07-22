36b6f2a72a998c0981827029bc634bd0ef7070dc
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-07-22 17:50:32 -0400

    refactor: Update getListStudents function to use ES6 syntax and improve code readability

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,24 +1,7 @@
-function getListStudents() {
-    // Define an array of student objects
-    const students = [
-      {
-        id: 1,
-        firstName: 'Guillaume',
-        location: 'San Francisco'
-      },
-      {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia'
-      },
-      {
-        id: 5,
-        firstName: 'Serena',
-        location: 'San Francisco'
-      }
+export default function getListStudents() {
+    return [
+        { id: 1, firstName: "Guillaume", location: "San Francisco" },
+        { id: 2, firstName: "James", location: "Columbia" },
+        { id: 5, firstName: "Serena", location: "San Francisco" }
     ];
-  
-    // Return the array of students
-    return students;
-  }
-  
+}
