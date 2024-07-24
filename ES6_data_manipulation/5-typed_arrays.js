6e0821aec825be4865c6e0b474413604d7cf5dc9
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-07-23 22:32:28 -0400

    refactor: Update createInt8TypedArray function to use DataView for improved code readability and error handling

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
-  }
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  int8View[position] = value;
-  return buffer;
+  const view = new DataView(buffer, 0);
+
+  if (position > length - 1) {
+    throw Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+  return view;
 }
