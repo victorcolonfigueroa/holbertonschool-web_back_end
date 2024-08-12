a537b490a52423fffd35a4c70734ec51ed2b1f44
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-12 14:47:00 -0400

    refactor: Add type annotations and improve documentation for the floor function

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,14 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 import math
 
 def floor(n: float) -> int:
+    """
+    Returns the largest integer less than or equal to a given number.
+
+    Args:
+        n (float): The number to be rounded down.
+
+    Returns:
+        int: The largest integer less than or equal to `n`.
+    """
     return math.floor(n)
