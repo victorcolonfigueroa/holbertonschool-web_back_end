db5a1c30d8dfab1ab7f8b7ca725f6b8031accbed
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-12 15:32:23 -0400

    refactor: Add type annotations and improve documentation for the add function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
-# sums floats and returns the result
+""" function that sums teh floats a and b """
 def add(a: float, b: float) -> float:
-    """
-    Adds two numbers and returns the result.
-
-    Args:
-        a (float): The first number.
-        b (float): The second number.
-
-    Returns:
-        float: The sum of the two numbers.
-    """
+    """returns result fo sum"""
     return a + b
