8a7cd830a04bde58f852536cb790da2527c4e93e
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-12 15:00:59 -0400

    refactor: Add type annotations and improve documentation for the floor function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to a string representation.
+
+    Args:
+        n (float): The float number to be converted.
+
+    Returns:
+        str: The string representation of the float number.
+    """
+    return str(n)
