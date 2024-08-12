2e85175438b1aa870fd2c70d93224cdc38ee0027
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-12 15:46:06 -0400

    refactor: Add type annotations and improve documentation for the sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from typing import List
-"""function that takes a list of floats """
+"""function that takes a list of floats"""
 
 
 def sum_list(input_list: List[float]) -> float:
