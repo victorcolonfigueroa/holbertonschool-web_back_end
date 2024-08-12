8b8776b662f5c5007efbb96ce50c2935dc53fecd
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-12 14:36:46 -0400

    a space in the comment

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-#takes 2 str and returns them as a concat string
+# takes 2 str and returns them as a concat string
 def concat(str1: str, str2: str) -> str:
     return str1 + str2
