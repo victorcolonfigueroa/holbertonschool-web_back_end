6e3a178ca51b2c4a09ad3287168998f8bb0a51c8
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-12 15:14:31 -0400

    refactor: Add type annotations and improve documentation for defining variables

---
+++ python_variable_annotations/4-define_variable_annotations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This module defines variables with type annotations.
+"""
+
+a: int = 1
+"""
+Variable 'a' represents an integer value.
+"""
+
+pi: float = 3.14
+"""
+Variable 'pi' represents a floating-point value.
+"""
+
+i_understand_annotations: bool = True
+"""
+Variable 'i_understand_annotations' represents a boolean value.
+"""
+
+school: str = "Holberton"
+"""
+Variable 'school' represents a string value.
+"""
