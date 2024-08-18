cf5814b35710873fd6e9d6e631b16920f2ae9911
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-18 08:57:28 -0400

    refactor: Improve type annotations and documentation for the element_length function

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,5 +1,16 @@
 from typing import List, Tuple
 
 
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    Calculate the length of each element in the given list.
+
+    Args:
+        lst (List[str]): The list of strings.
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples where each tuple contains a string from the input list and its length.
+    """
+    return [(i, len(i)) for i in lst]
 def  element_length(lst: List[str]) -> List[Tuple[str, int]]:
     return [(i, len(i)) for i in lst]
