e8c4686afd16893a57632816ef19794267f109b6
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-08-21 13:47:00 -0400

    refactor: Simplify index_range function and improve documentation

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -42,16 +42,7 @@ class Server:
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Returns the start and end indices for a given page and page size.
-
-    Args:
-        page (int): The page number.
-        page_size (int): The number of items per page.
-
-    Returns:
-        tuple: A tuple containing the start and end indices.
-    """
+    """return pages """
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
