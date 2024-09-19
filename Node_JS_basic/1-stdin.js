c7ab38a63202c671a5aab5a645d273f3c2af8d83
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-09-18 21:42:57 -0400

    refactor: Update stdin.js to fix typo in console.log message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-console.log("Welcome to Holberton School, what is your naem?");
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write("Your name is: $(chunk)");
-    }
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    console.log("This important software")
+  console.log('This important software is now closing');
 });
