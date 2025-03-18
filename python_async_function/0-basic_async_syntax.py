12b5606717cf6a71fe7c281fa5857d05338b8aae
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2025-03-18 11:51:13 -0400

    enhance wait_random function docstring for clarity and detail

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,12 +3,23 @@
 """this async coroutine takes an int and waits for random delay"""
 
 
-import asyncio
 import random
+import asyncio
 
 
+# function
 async def wait_random(max_delay: int = 10) -> float:
-    """returns random delay"""
-    rand_delay = random.uniform(0, max_delay)
-    await asyncio.sleep(rand_delay)
-    return rand_delay
+    """
+    Asynchronous coroutine that waits for a random delay
+    between 0 and max_delay
+    (included and float value) seconds and eventually returns it.
+
+    Args:
+        max_delay (int, optional): The maximum delay. Defaults to 10.
+
+    Returns:
+        int: The actual delay used.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
